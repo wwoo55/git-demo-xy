c2675aef1e00b2b5b0c9675b3477a247936e5e35
xy <xiaoyudeyo@foxmail.com>
2021-05-06 10:55:25 +0800

    first

---
+++ src/main/java/com/biz/mvndemo/MvnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.biz.mvndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biz/mvndemo/MvnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.mvndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
